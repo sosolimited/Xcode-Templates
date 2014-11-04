13d3c1655511bfca333c21de10470a75203fd7b7
David Wicks <david@sosolimited.com>
2014-11-04 16:22:36 -0500

    First rough templates.

---
+++ soso/Empty Class.xctemplate/WithHeader/___FILEBASENAME___.h
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#pragma once
+
+namespace ns
+{
+
+class ___FILEBASENAMEASIDENTIFIER___
+{
+public:
+
+private:
+
+};
+
+} // namespace ns
