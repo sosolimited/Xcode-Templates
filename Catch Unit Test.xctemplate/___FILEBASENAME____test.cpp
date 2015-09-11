b2524b4b75f10c61dc388aa241cc35594fe3fc17
David Wicks <david@sosolimited.com>
2015-09-11 09:56:57 -0400

    Unit test template.

---
+++ Catch Unit Test.xctemplate/___FILEBASENAME____test.cpp
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "catch.hpp"
+
+TEST_CASE("___FILEBASENAME___")
+{
+	auto a = 0;
+	auto b = 1;
+
+	SECTION("Write a failing test first to confirm your new test is run.")
+	{
+		REQUIRE(a == b);
+	}
+}
