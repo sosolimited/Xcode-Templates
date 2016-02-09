daebea481d2b2102c72929741f2bdf2561ab4cb4
David Wicks <david@sosolimited.com>
2016-02-09 11:38:36 -0500

    Catch unit test main template option.

---
+++ Catch Unit Test.xctemplate/IsMain/___FILEBASENAME___.cpp
@@ -0,0 +1,14 @@
+//
+// ___FILENAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// ___COPYRIGHT___
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// This file builds the Catch test-runner.
+// Tests can be added in separate .cpp files as TEST_CASES.
+// If you don't have catch in your project already, run the following shell script:
+// curl https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp > catch.hpp
